1fadc08fc95da9e7dd183ccddf37a5b568d60b02
Neeraj957 <neerajnani.14@gmail.com>
2023-12-08 01:23:59 +0530

    adding footers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>This is edited by Neeraj.</h1>
+            <Footer/>
     </div>
   );
 }

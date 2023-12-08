063fb3368855497bd922bed183c8e177aa18a231
Neeraj957 <neerajnani.14@gmail.com>
2023-12-08 16:36:30 +0530

    contact component and route in app.js file

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import { Footer } from './components/Footer';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import { NavigationBar } from './components/NavigationBar';
+import ContactUs from './components/ContactUs.jsx'
 
 
 function App() {
   return (
     <BrowserRouter>
 
-      
-    
+
+
 
       <NavigationBar></NavigationBar>
-      <Carousels></Carousels>
-     
-      <Footer/>
+      <Routes>
+        <Route path="/contact-us" element={<ContactUs/>}></Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
 
 
